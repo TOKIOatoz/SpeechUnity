ca4ba79886edb53a2ce067a01d403e2d1b670f94
TOKIOatoz <tokioatoz1@gmail.com>
2021-12-31 07:48:47 +0900

    Add _newLine and _deleteRow

--- SpeechTest/Assets/_Assets/Speech/Script/GetPhraseToText.cs
+++ SpeechTest/Assets/_Assets/Speech/Script/GetPhraseToText.cs
@@ -37,13 +37,16 @@ public class GetPhraseToText : MonoBehaviour
 
     private string _tentativeSubstitutionText = "";
 
-    //for文用
-    private int _num0, _num1;
+    //for文, while分用
+    private int _num0, _num1, _countNum;
+
+    //削除する文字数
+    private int _removeLength;
 
     //表示するテキスト
     public Text TextToUI {get; private set;}
-    private int _wordLength = 0, _pastTextLength = 0, _tentativeRow, _correctRow, _removeRow;
-    private int[] _removeLength = new int[10];
+    private int _pastTextLength = 0, _tentativeRow, _correctRow, _removeRow;
+
     void Start()
     {
         //テキスト初期化
@@ -85,82 +88,129 @@ public class GetPhraseToText : MonoBehaviour
     {
         //stringBuilder経由で仮代入
         _stringBuilder.Clear();
-        _stringBuilder.Append(_pastText);
-        _stringBuilder.Append(" ");
+        if (_pastText.Length > 0)
+        {
+            _stringBuilder.Append(_pastText);
+            _stringBuilder.Append(' ');
+            _stringBuilder.Replace('\n', ' ');
+        }
         _stringBuilder.Append(TentativeText);
         _tentativeSubstitutionText = _stringBuilder.ToString();
 
-        //行数に応じて削除
-        _tentativeRow = (int)(_tentativeSubstitutionText.Length / (_maxTextLength - _maxBlank));
-        if (_tentativeRow > 0)
+        //文字数に応じて改行及び行削除
+        if (_tentativeSubstitutionText.Length > _maxTextLength)
         {
-            _correctRow = 0;
-            _pastTextLength = 0;
-            for (_num0 = 0; _num0 <= _tentativeRow; _num0++)
-            {
-                _correctRow++;
-                if ((_pastTextLength + _maxTextLength - _maxBlank) <= _tentativeSubstitutionText.Length)
-                {
-                    for (_num1 = _maxBlank; _num1 > 0; _num1--)
-                    {
-                        //TODO Fix
-                        //IndexOutOfRangeException: Index was outside the bounds of the array.
-                        if (_tentativeSubstitutionText[_pastTextLength + _maxTextLength - _num1] == ' ' && _tentativeSubstitutionText[_pastTextLength + _maxTextLength - _num1] == '\n')
-                        {
-                            _stringBuilder.Insert(_pastTextLength + _maxTextLength - _num1, "\n");
-                            _stringBuilder.Remove(_pastTextLength + _maxTextLength - _num1, 1);
-                            _pastTextLength = _pastTextLength + _maxTextLength - _num1;
-                            _removeLength[_num0] = _pastTextLength;
-                            break;
-                        }
-                        else if (_num1 == 1)
-                        {
-                            _stringBuilder.Insert(_pastTextLength + _maxTextLength - _num1, "\n");
-                            _pastTextLength = _pastTextLength + _maxTextLength;
-                            _removeLength[_num0] = _pastTextLength;
-                        }
-                    }
-                }
-                else
-                {
-                    break;
-                }
-            }
+            //改行追加
+            _newLine();
+
+            //改行後代入
+            _tentativeSubstitutionText = _stringBuilder.ToString();
+
+            //削除行数確認
             _removeRow = _correctRow - _maxRow;
             if (_removeRow > 0)
             {
-                _stringBuilder.Remove(0, _removeLength[_removeRow - 1]);
+                //行削除
+                _deleteRow();
             }
         }
 
         TextToUI.text = _stringBuilder.ToString();
-        _wordLength = TentativeText.Length + 1;
     }
 
     //テキストをセット
     private void _setText()
     {
+        //stringBuilder経由で仮代入
         _stringBuilder.Clear();
-        _stringBuilder.Append(_pastText);
-        _stringBuilder.Append(" ");
+        if (_pastText.Length > 0)
+        {
+            _stringBuilder.Append(_pastText);
+            _stringBuilder.Append(' ');
+            _stringBuilder.Replace('\n', ' ');
+        }
         _stringBuilder.Append(TentativeText);
+        _tentativeSubstitutionText = _stringBuilder.ToString();
+
+        //文字数に応じて改行及び行削除
+        if (_tentativeSubstitutionText.Length > _maxTextLength)
+        {
+            //改行追加
+            _newLine();
+
+            //改行後代入
+            _tentativeSubstitutionText = _stringBuilder.ToString();
+
+            //TODO _correctRowが間違ってる場合が生じるので直す
+            Debug.Log(_correctRow);
+
+            //削除行数確認
+            _removeRow = _correctRow - _maxRow;
+            if (_removeRow > 0)
+            {
+                //行削除
+                _deleteRow();
+            }
+        }
         TextToUI.text = _stringBuilder.ToString();
-        _wordLength = 0;
-
-
         _pastText = TextToUI.text;
+
+        Debug.Log(_pastText);
     }
 
     //改行入力
     private void _newLine()
     {
-
+        //想定される最大行数
+        _tentativeRow = Mathf.CeilToInt((_tentativeSubstitutionText.Length - _maxTextLength + _maxBlank) / _maxTextLength) + 1;
+        
+        //実際の行数を加算していく変数のリセット
+        _correctRow = 0;
+        _pastTextLength = 0;
+        
+        for (_num0 = 0; _num0 < _tentativeRow; _num0++)
+        {
+            _correctRow++;
+            if ((_pastTextLength + _maxTextLength) < _tentativeSubstitutionText.Length)
+            {
+                for (_num1 = _maxBlank; _num1 > 0; _num1--)
+                {
+                    if (_tentativeSubstitutionText[_pastTextLength + _maxTextLength - _num1] == ' ')
+                    {
+                        _stringBuilder.Remove(_pastTextLength + _maxTextLength - _num1, 1);
+                        _stringBuilder.Insert(_pastTextLength + _maxTextLength - _num1, '\n');
+                        _pastTextLength = _pastTextLength + _maxTextLength - _num1 + 1;
+                        break;
+                    }
+                    else if (_num1 == 1)
+                    {
+                        _stringBuilder.Insert(_pastTextLength + _maxTextLength, "\n");
+                        _pastTextLength = _pastTextLength + _maxTextLength + 1;
+                    }
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 
     //行削除
     private void _deleteRow()
     {
-        
+        //削除する行数の判定をし，その行数分の\nに到達するまでの先頭からの文字数を数え，その分を削除
+        _countNum = 0;
+        _removeLength = 0;
+        while (_countNum < _removeRow && _removeLength < _tentativeSubstitutionText.Length)
+        {
+            if (_tentativeSubstitutionText[_removeLength] == '\n')
+            {
+                _countNum++;
+            }
+            _removeLength++;
+        }
+        _stringBuilder.Remove(0, _removeLength);
     }
 
     //WindowsSpeechが聞き取り中に仮テキストを更新
